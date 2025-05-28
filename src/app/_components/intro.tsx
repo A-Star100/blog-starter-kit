dc0bbb2250ec1b85380926371af4cec3222cd162
Anirudh Sevugan <sonicforces207@gmail.com>
2025-05-27 20:36:36 -0500

    Update intro.tsx

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -4,10 +4,10 @@ export function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Tech News
+        CodeArticles
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        You can rely on us for all things tech
+        Just coding articles. That's it.
       </h4>
     </section>
   );
